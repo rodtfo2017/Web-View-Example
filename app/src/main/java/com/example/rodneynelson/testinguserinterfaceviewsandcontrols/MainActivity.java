8037bff49925e3ed4bf502b003f90c4855842084
Rodney Nelson <rodneynelson@rodneys-mbp.attlocal.net>
2017-08-04 00:06:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rodneynelson/testinguserinterfaceviewsandcontrols/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rodneynelson.testinguserinterfaceviewsandcontrols;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        webview.loadUrl("http://www.ibm.com/");
+
+
+
+    }
+}
